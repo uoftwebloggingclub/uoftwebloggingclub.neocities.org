bb96a96a2d59d34757c59ed90e031da93973f81d
numberisnan <github@farazkaleemmalik.cyou>
2024-12-11 22:46:42 -0500

    Build feed collection and install dependencies in build script<

--- runlocal.sh
+++ runlocal.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+if [[ ! $(ruby --version) ]]; then 
+    echo "Ruby not installed!"
+elif [[ ! $(bundle --version) ]]; then
+    echo "Bundle not installed!"
+fi
+
+if [[ ! $(bundle check) ]]; then
+    bundle install
+fi
+
+if [[ ! -d "_recent_feeds" ]]; then
+    echo "Generating feed metadata"
+    ruby generate_feed_includes.rb
+fi
+
 # See https://github.com/jitinnair1/gradfolio/wiki/Local-Development
 bundle exec jekyll serve --trace
