568178a1bb7e42f4bbf06fdeacadf4f8344db5a6
numberisnan <github@farazkaleemmalik.cyou>
2024-12-17 04:35:54 -0500

    Sort _feed_includes ourselves + bump preview count to 10

--- generate_feed_includes.rb
+++ generate_feed_includes.rb
@@ -10,7 +10,8 @@ feed = FeedNormalizer::FeedNormalizer.parse(feed_text)
 FileUtils.rm_rf(dir_name) # This is where the collection lives
 Dir.mkdir(dir_name)
 
-feed.entries.first(5).each_with_index do |item, i| # Only do the first 5
+# rss-combine doesn't properly sort entries so we have to do that ourselves
+feed.entries.sort_by { |item| item.last_updated }.reverse.first(10).each_with_index do |item, i| # Only do the first 10
     File.open(dir_name + "/" + i.to_s + ".html", 'w') do |f|
         f.write("---\ntitle: #{item.title}\nlink: #{item.url}\n---\n#{item.content}")
     end
