f025f065707315888c075470833c1fec80160d26
numberisnan <github@farazkaleemmalik.cyou>
2024-12-26 15:18:02 -0500

    Add 'description' tag to _recent_feeds

--- generate_feed_includes.rb
+++ generate_feed_includes.rb
@@ -13,6 +13,6 @@ Dir.mkdir(dir_name)
 # rss-combine doesn't properly sort entries so we have to do that ourselves
 feed.entries.sort_by { |item| item.last_updated }.reverse.first(10).each_with_index do |item, i| # Only do the first 10
     File.open(dir_name + "/" + i.to_s + ".html", 'w') do |f|
-        f.write("---\ntitle: #{item.title}\nlink: #{item.url}\n---\n#{item.content}")
+        f.write("---\ntitle: #{item.title}\nlink: #{item.url}\ndescription: #{item.description}\n---\n#{item.content}")
     end
 end
