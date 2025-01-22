804962c67fda5116c4097ab0d1a40f354ec6e0bd
numberisnan <github@farazkaleemmalik.cyou>
2025-01-21 23:21:24 -0500

    Make YAML Generation more robust; Limit preview to 10 in template

--- generate_feed_includes.rb
+++ generate_feed_includes.rb
@@ -1,5 +1,6 @@
 require 'feed-normalizer'
 require 'fileutils'
+require 'yaml'
 
 dir_name = "_recent_feeds"
 feed_name = "./feed.xml"
@@ -11,8 +12,12 @@ FileUtils.rm_rf(dir_name) # This is where the collection lives
 Dir.mkdir(dir_name)
 
 # rss-combine doesn't properly sort entries so we have to do that ourselves
-feed.entries.sort_by { |item| item.last_updated }.reverse.first(10).each_with_index do |item, i| # Only do the first 10
+feed.entries.sort_by { |item| item.last_updated }.reverse.each_with_index do |item, i|
     File.open(dir_name + "/" + i.to_s + ".html", 'w') do |f|
-        f.write("---\ntitle: #{item.title}\nlink: #{item.url}\ndescription: #{item.description}\n---\n#{item.content}")
+        metadata = {}
+        metadata['title'] = item.title
+        metadata['link'] = item.url
+        metadata['description'] = item.description
+        f.write("#{metadata.to_yaml}---\n#{item.content}")
     end
 end
