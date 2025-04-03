7ac28167e34c780fd4a121cd1e40fdc6bc8255e1
numberisnan <github@farazkaleemmalik.cyou>
2025-04-02 22:30:24 -0400

    Delete _recent_feeds in favor of more consistent json format

--- scripts/generate_feed_includes.rb
+++ scripts/generate_feed_includes.rb
@@ -1,27 +1,28 @@
 require 'feed-normalizer'
 require 'fileutils'
-require 'yaml'
+require 'json'
 require 'uri/http'
 
-dir_name = "_recent_feeds"
+data_dir_name = "_data"
 feed_name = "./feed.xml"
 
 feed_text = File.read(feed_name) # Read and parse feed
 feed = FeedNormalizer::FeedNormalizer.parse(feed_text)
 
-FileUtils.rm_rf(dir_name) # This is where the collection lives
-Dir.mkdir(dir_name)
-
 # rss-combine doesn't properly sort entries so we have to do that ourselves
+feedjson = []
 feed.entries.sort_by { |item| item.last_updated }.reverse.each_with_index do |item, i|
-    File.open(dir_name + "/" + i.to_s + ".html", 'w') do |f|
-        metadata = {}
-        metadata['title'] = item.title
-        metadata['link'] = item.url
-        metadata['description'] = item.description
-        metadata['date'] = item.last_updated
-        metadata['host'] = URI.parse(item.url).host
+    metadata = {}
+    metadata['title'] = item.title
+    metadata['link'] = item.url
+    metadata['description'] = item.description
+    metadata['date'] = item.last_updated.to_i
+    metadata['host'] = URI.parse(item.url).host
+    metadata['content'] = item.content
 
-        f.write("#{metadata.to_yaml}---\n#{item.content}")
-    end
+    feedjson.push(metadata)
+end
+
+File.open(data_dir_name + "/feed.json", "w") do |f|
+    f.write(JSON.generate(feedjson))
 end
